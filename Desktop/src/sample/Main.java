c48d9140a4d03c394c754a81d66bdd98f1633361
Neizhko <v.v.neizhko@gmail.com>
2020-12-09 22:46:55 +0300

    Added simple GUI implementation.

--- Desktop/src/sample/Main.java
+++ Desktop/src/sample/Main.java
@@ -6,17 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("gui/sample.fxml"));
+        primaryStage.setTitle("I2PSexChat");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
