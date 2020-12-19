429286e98a24cc9568564d05b96bc7d4badb0c3e
Augustussssss <rustam3799@gmail.com>
2020-12-19 16:38:34 +0300

    Fixed foreign key interaction, added connection method.

--- Desktop/src/sample/Database/Database.java
+++ Desktop/src/sample/Database/Database.java
@@ -1,27 +1,49 @@
 package sample.Database;
 
+import org.sqlite.SQLiteConfig;
+
 import java.sql.*;
 import java.text.MessageFormat;
 
 public class Database {
-    private String url;
 
-    public void create_database(String fileName) {
-
+    private Connection connect() {
         String dir = System.getProperty("user.dir");
-         this.url = "jdbc:sqlite:" + dir + "\\src\\sample\\Database\\sql_database\\" + fileName;
+        String url = "jdbc:sqlite:" + dir + "\\src\\sample\\Database\\sql_database\\sec_chat_database.db";
 
-        try (Connection conn = DriverManager.getConnection(this.url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-            }
+        SQLiteConfig config = new SQLiteConfig();
+        config.enforceForeignKeys(true);
 
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(url, config.toProperties());
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-    };
+        return  conn;
+    }
+
+//    public void create_database(String fileName) {
+//
+//        String dir = System.getProperty("user.dir");
+//         this.url = "jdbc:sqlite:" + dir + "\\src\\sample\\Database\\sql_database\\" + fileName;
+//
+//        SQLiteConfig config = new SQLiteConfig();
+//        config.enforceForeignKeys(true);
+//
+//        try (Connection conn = DriverManager.getConnection(this.url, config.toProperties())) {
+//            if (conn != null) {
+//                DatabaseMetaData meta = conn.getMetaData();
+//                Statement s = conn.createStatement(); s.executeUpdate("PRAGMA foreign_keys = ON");
+//                System.out.println("The driver name is " + meta.getDriverName());
+//                System.out.println("A new database has been created.");
+//            }
+//
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    };
 
     public void create_all_tables() {
         create_user_table();
@@ -48,7 +70,7 @@ public class Database {
                 + "`private_key` char(50) not null,\n"
                 + "`info` char(100)\n"
                 + ");";
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              Statement statement = connection.createStatement()) {
             statement.execute(create_user_table);
         } catch (SQLException e) {
@@ -64,7 +86,7 @@ public class Database {
                 + "`delete_message_time` int,\n"
                 + "`aes_key` char(50)\n"
                 + ");";
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              Statement statement = connection.createStatement()) {
             statement.execute(create_room_table);
         } catch (SQLException e) {
@@ -76,10 +98,10 @@ public class Database {
         String create_rooms_table = "create table if not exists `rooms` (\n"
                 + "`room_id` int not null,\n"
                 + "`user_id` int not null,\n"
-                + "foreign key (`room_id`) references `room` (`id`) on delete cascade on update cascade,\n"
-                + "foreign key (`user_id`) references `user` (`id`) on delete cascade on update cascade\n"
+                + "constraint `fk_room_id` foreign key (`room_id`) references `room` (`id`) on delete cascade on update cascade,\n"
+                + "constraint `fk_user_id`foreign key (`user_id`) references `user` (`id`) on delete cascade on update cascade\n"
                 + ");";
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              Statement statement = connection.createStatement()) {
             statement.execute(create_rooms_table);
         } catch (SQLException e) {
@@ -97,7 +119,7 @@ public class Database {
                 + "foreign key (`room_id`) references `room` (`id`) on delete cascade on update cascade,\n"
                 + "foreign key (`user_id`) references `user` (`id`) on delete cascade on update cascade\n"
                 + ");";
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              Statement statement = connection.createStatement()) {
             statement.execute(create_message_table);
         } catch (SQLException e) {
@@ -107,7 +129,7 @@ public class Database {
 
     public void add_user(String name, String private_key, String info) {
         String add_user = "insert into user values (null, ?, ?, ?)";
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
         PreparedStatement preparedStatement = connection.prepareStatement(add_user)) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, private_key);
@@ -120,7 +142,7 @@ public class Database {
 
     public void add_room(String name, int delete_message_time, String info, String aes_key) {
         String add_room = "insert into room values (null, ?, ?, ?, ?)";
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              PreparedStatement preparedStatement = connection.prepareStatement(add_room)) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, info);
@@ -134,7 +156,7 @@ public class Database {
 
     public void register_message(int room_id, int user_id, String text, String date) {
         String add_room = "insert into message values (null, ?, ?, ?, ?)";
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              PreparedStatement preparedStatement = connection.prepareStatement(add_room)) {
             preparedStatement.setInt(1, room_id);
             preparedStatement.setInt(2, user_id);
@@ -148,7 +170,7 @@ public class Database {
 
     public void add_user_to_room(int room_id, int user_id) {
         String add_room = "insert into rooms values (?, ?)";
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              PreparedStatement preparedStatement = connection.prepareStatement(add_room)) {
             preparedStatement.setInt(1, room_id);
             preparedStatement.setInt(2, user_id);
@@ -160,7 +182,7 @@ public class Database {
 
     private void print_users() {
         String get_table_values = MessageFormat.format("select * from {0}", "user");
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              Statement statement = connection.createStatement();
              ResultSet result = statement.executeQuery(get_table_values)) {
 
@@ -178,7 +200,7 @@ public class Database {
 
     private void print_rooms() {
         String get_table_values = MessageFormat.format("select * from {0}", "room");
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              Statement statement = connection.createStatement();
              ResultSet result = statement.executeQuery(get_table_values)) {
 
@@ -197,7 +219,7 @@ public class Database {
 
     private void print_messages() {
         String get_table_values = MessageFormat.format("select * from {0}", "message");
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              Statement statement = connection.createStatement();
              ResultSet result = statement.executeQuery(get_table_values)) {
 
@@ -216,7 +238,7 @@ public class Database {
 
     private void print_rooms_users() {
         String get_table_values = MessageFormat.format("select * from {0}", "rooms");
-        try (Connection connection = DriverManager.getConnection(this.url);
+        try (Connection connection = this.connect();
              Statement statement = connection.createStatement();
              ResultSet result = statement.executeQuery(get_table_values)) {
 
@@ -231,9 +253,9 @@ public class Database {
     }
 
     public void delete_user(int user_id) {
-        String add_room = "delete from user where id = ?";
-        try (Connection connection = DriverManager.getConnection(this.url);
-             PreparedStatement preparedStatement = connection.prepareStatement(add_room)) {
+        String delete_user = "delete from user where id = ?";
+        try (Connection connection = this.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(delete_user)) {
             preparedStatement.setInt(1, user_id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -242,9 +264,9 @@ public class Database {
     }
 
     public void delete_room(int room_id) {
-        String add_room = "delete from room where id = ?";
-        try (Connection connection = DriverManager.getConnection(this.url);
-             PreparedStatement preparedStatement = connection.prepareStatement(add_room)) {
+        String delete_room = "delete from room where id = ?";
+        try (Connection connection = this.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(delete_room)) {
             preparedStatement.setInt(1, room_id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -253,11 +275,21 @@ public class Database {
     }
 
     public void change_user_info(int user_id, String new_info) {
-        String add_room = "update user set info = ? where id = ?";
-        try (Connection connection = DriverManager.getConnection(this.url);
-             PreparedStatement preparedStatement = connection.prepareStatement(add_room)) {
+        String change_user_info = "update user set info = ? where id = ?";
+        try (Connection connection = this.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(change_user_info)) {
             preparedStatement.setString(1, new_info);
             preparedStatement.setInt(2, user_id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void drop_table() {
+        String drop_table = "drop table room";
+        try (Connection connection = this.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(drop_table)) {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
