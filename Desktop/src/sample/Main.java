e6c842129eca23f8e130ed6d304f09691c60165a
Razuvaev_DD <Razuvaev_DD@mail.ru>
2020-12-20 20:22:23 +0300

    Added full I2P connection stack

--- Desktop/src/sample/Main.java
+++ Desktop/src/sample/Main.java
@@ -9,8 +9,14 @@ import javafx.stage.Stage;
 import sample.Database.Database;
 import sample.Encryptor.Encryptor;
 import sample.Encryptor.LoginData;
+import sample.I2PConnector.I2PConnector;
+import sample.Objects.Account;
+import sample.Objects.Message;
+import sample.Objects.TypeOfMessage;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -27,9 +33,9 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
-        Database db = new Database();
-        List<String> users = db.getUsersInRoom(1);
-        System.out.println(users);
+//        Database db = new Database();
+//        List<String> users = db.getUsersInRoom(1);
+//        System.out.println(users);
 
 //        LoginData data = new LoginData("Login", "Password");
 //        try {
@@ -38,6 +44,30 @@ public class Main extends Application {
 //        } catch (Exception e) {
 //            System.err.println(e.getMessage());
 //        }
+
+
+
+        I2PConnector i2pConnector = new I2PConnector();
+
+        System.out.println("Отправляем сообщение");
+        i2pConnector.sendMessage(new Message(
+                i2pConnector.getMyAccount(),
+                i2pConnector.getMyAccount(),
+                "testMsgFromClient", TypeOfMessage.StringMessage));
+
+        System.out.println("Ждем пока что-то появится");
+        // зависаем пока нет сообщений
+        while (!(i2pConnector.haveNewMessages())) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //получаем буфер писем (с отчисткой!)
+        ArrayList<Message> messages = i2pConnector.getNewMessages();
+        System.out.println(messages.get(0).type==TypeOfMessage.StringMessage?messages.get(0).message:"Не текст");
 
         launch(args);
     }
