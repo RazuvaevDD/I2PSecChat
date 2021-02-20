bca341a6a60058ea88b9ed4930d7ff771baf8632
RazuvaevDD <Razuvaev_DD@mail.ru>
2021-02-20 19:24:18 +0300

    Supporting HTTP without getting I2P account

--- Desktop/src/sample/I2PConnector/I2PConnector.java
+++ Desktop/src/sample/I2PConnector/I2PConnector.java
@@ -3,6 +3,9 @@ package sample.I2PConnector;
 import sample.Objects.Account;
 import sample.Objects.Message;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class I2PConnector {
@@ -33,7 +36,8 @@ public class I2PConnector {
      */
     public I2PConnector(TypeOfConnection connectionType){
         this.connectionType = connectionType;
-        i2pServer.start();
+        if(connectionType==TypeOfConnection.I2PConnection)
+            i2pServer.start();
         Account myAccount = getMyAccount();
         new HTTPService(myAccount);
     }
@@ -83,19 +87,75 @@ public class I2PConnector {
     Возвращает аккаунт пользователя или зависает, ожидая включения сервера.
      */
     public static Account getMyAccount() {
-        while(i2pServer.getMyDestination().isEmpty()){
-            try {
-                System.out.println("[INFO] I2PConnector: Ожидаем пока маршрутизатор строит туннели...");
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        if(connectionType==TypeOfConnection.I2PConnection){
+            while(i2pServer.getMyDestination().isEmpty()){
+                try {
+                    System.out.println("[INFO] I2PConnector: Ожидаем пока маршрутизатор строит туннели...");
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if(!i2pServer.getMyDestination().isEmpty()){
+                    System.out.println("[INFO] I2PConnector: Туннель выделен!");
+                }
+            };
+            Account account = new Account("My Account I2P", i2pServer.getMyDestination());
+            return account;
+        }
+        else{
+            if(isWindows()) {
+                try {
+                    String command = "wmic csproduct get UUID";
+                    StringBuffer output = new StringBuffer();
+
+                    Process SerNumProcess = Runtime.getRuntime().exec(command);
+                    BufferedReader sNumReader = new BufferedReader(new InputStreamReader(SerNumProcess.getInputStream()));
+
+                    while (true) {
+                        String line = "";
+                        if (!((line = sNumReader.readLine()) != null)) break;
+
+                        output.append(line + "\n");
+                    }
+                    String MachineID = output.toString().substring(output.indexOf("\n"), output.length()).trim();
+                    ;
+                    System.out.println(MachineID);
+                    return new Account("My Windows Account HTTP", MachineID);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            if(!i2pServer.getMyDestination().isEmpty()){
-                System.out.println("[INFO] I2PConnector: Туннель выделен!");
+            if(isUnix()){
+                System.err.println("[CRITICAL_ERROR] I2PConnector: Не реализовано для Linux!");
+                System.exit(-1);
             }
-        };
-        Account account = new Account("My Account", i2pServer.getMyDestination());
-        return account;
+            System.err.println("[CRITICAL_ERROR] I2PConnector: Не обнаружен идентификатор устройства!");
+            System.exit(-1);
+
+            return null;
+        }
+    }
+
+    private static boolean isWindows(){
+        String os = System.getProperty("os.name").toLowerCase();
+        //windows
+        return (os.indexOf( "win" ) >= 0);
+    }
+
+    private static boolean isMac(){
+        String os = System.getProperty("os.name").toLowerCase();
+        //Mac
+        return (os.indexOf( "mac" ) >= 0);
+    }
+
+    private static boolean isUnix (){
+        String os = System.getProperty("os.name").toLowerCase();
+        //linux or unix
+        return (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0);
+    }
+    private static String getOSVerion() {
+        String os = System.getProperty("os.version");
+        return os;
     }
 }
 
